571b208b4c2f6636686839d3fb52fa9b9b5d1a25
Xiaofan Zhou <xiaofanzhou@microsoft.com>
2025-07-10 17:20:40 +0800

    revert changes

--- src/Commands/CommandFactory.cs
+++ src/Commands/CommandFactory.cs
@@ -92,7 +92,7 @@ public class CommandFactory
             }
         }
 
-        if (commandsFromGroups.Count == 0 && !groupNames.Any(i => i =="codetocloud"))
+        if (commandsFromGroups.Count == 0)
         {
             throw new KeyNotFoundException($"No valid group in '[{string.Join(",", groupNames)}]' found in command groups.");
         }
