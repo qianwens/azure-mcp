0da1f0225fafc3d33de977e10945582022390f1d
xiaofanzhou <xiaofanzhou@microsoft.com>
2025-07-15 15:23:35 +0800

    clean

--- src/Areas/Deploy/Commands/DeployJsonContext.cs
+++ src/Areas/Deploy/Commands/DeployJsonContext.cs
@@ -18,7 +18,6 @@ namespace AzureMcp.Areas.Deploy.Commands;
 [JsonSerializable(typeof(AppTopology))]
 [JsonSerializable(typeof(MermaidData))]
 [JsonSerializable(typeof(MermaidConfig))]
-[JsonSerializable(typeof(CognitiveServiceProperties))]
 [JsonSerializable(typeof(QuotaCheckCommand.QuotaCheckCommandResult))]
 [JsonSerializable(typeof(QuotaInfo))]
 [JsonSerializable(typeof(Dictionary<string, List<QuotaInfo>>))]
