025c8742f21438d138566b33d1f7aa9d6fac687b
Chi Wang <wchi@microsoft.com>
2025-07-22 13:56:27 +0800

    fix and test

--- src/Areas/Deploy/Commands/GenerateMermaidChart.cs
+++ src/Areas/Deploy/Commands/GenerateMermaidChart.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Collections.Immutable;
 using System.Text;
 using AzureMcp.Areas.Deploy.Options;
 using Microsoft.Extensions.ObjectPool;
@@ -54,38 +55,38 @@ public static class GenerateMermaidChart
 
             services.Add(CreateComponentName(serviceInternalName, string.Join("\n", serviceName), "service", NodeShape.Rectangle));
 
-            relationships.Add(CreateRelationshipString(serviceInternalName, service.Name, "hosted on", ArrowType.Solid));
+            relationships.Add(CreateRelationshipString(serviceInternalName, $"{FlattenServiceType(service.AzureComputeHost)}_{service.Name}", "hosted on", ArrowType.Solid));
         }
 
         var aksClusterExists = false;
         foreach (var service in appTopology.Services)
         {
-
+            string serviceResourceInternalName = $"{FlattenServiceType(service.AzureComputeHost)}_{service.Name}";
             if (service.AzureComputeHost == "aks")
             {
                 if (!aksClusterExists)
                 {
                     // Add AKS cluster as a subgraph
-                    resources.Add($"subgraph {aksClusterInternalName} [{aksClusterName}]");
+                    resources.Add($"subgraph {aksClusterInternalName} [\"{aksClusterName}\"]");
                     // containerized services share the same AKS cluster
                     foreach (var aksservice in appTopology.Services.Where(s => s.AzureComputeHost == "aks"))
                     {
                         resources.Add(CreateComponentName($"{aksservice.AzureComputeHost}_{aksservice.Name}", $"{aksservice.Name} (Containerized Service)", "compute", NodeShape.RoundedRectangle));
                     }
                     resources.Add("end");
-                    resources.Add($"class {aksClusterInternalName}:::cluster");
+                    resources.Add($"{aksClusterInternalName}:::cluster");
                     aksClusterExists = true;
                 }
             }
             // each service should have a compute resource type
             else
             {
-                resources.Add(CreateComponentName($"{FlattenServiceType(service.AzureComputeHost)}_{service.Name}", $"{service.Name} ({service.AzureComputeHost})", "compute", NodeShape.RoundedRectangle));
+                resources.Add(CreateComponentName(serviceResourceInternalName, $"{service.Name} ({GetFormalName(service.AzureComputeHost)})", "compute", NodeShape.RoundedRectangle));
             }
             foreach (var dependency in service.Dependencies)
             {
                 var instanceInternalName = $"{FlattenServiceType(dependency.ServiceType)}.{dependency.Name}";
-                var instanceName = $"{dependency.Name} ({dependency.ServiceType})";
+                var instanceName = $"{dependency.Name} ({GetFormalName(dependency.ServiceType)})";
 
                 if (IsComputeResourceType(dependency.ServiceType))
                 {
@@ -99,7 +100,7 @@ public static class GenerateMermaidChart
                     resources.Add(CreateComponentName(instanceInternalName, instanceName, "binding", NodeShape.Circle));
                 }
 
-                relationships.Add(CreateRelationshipString(service.Name, instanceInternalName, dependency.ConnectionType, ArrowType.Dotted));
+                relationships.Add(CreateRelationshipString(serviceResourceInternalName, instanceInternalName, dependency.ConnectionType, ArrowType.Dotted));
             }
         }
 
@@ -154,6 +155,16 @@ public static class GenerateMermaidChart
         };
     }
 
+    private static string GetFormalName(string name)
+    {
+        if (ResourceTypeConverter.TryGetValue(name, out var formalName))
+        {
+            return formalName;
+        }
+        return name;
+
+    }
+
     private static string FlattenServiceType(string serviceType)
     {
         return serviceType.ToLowerInvariant().Replace("azure", "");
@@ -163,6 +174,30 @@ public static class GenerateMermaidChart
     {
         return Enum.GetNames<AzureServiceConstants.AzureComputeServiceType>().Contains(serviceType, StringComparer.OrdinalIgnoreCase);
     }
+
+    private static IDictionary<string, string> ResourceTypeConverter = new Dictionary<string, string>
+    {
+        { "appservice", "Azure App Service" },
+        { "containerapp", "Azure Container Apps" },
+        { "functionapp", "Azure Functions" },
+        { "staticwebapp", "Azure Static Web Apps" },
+        { "aks", "Azure Kubernetes Services" },
+        { "azureaisearch", "Azure AI Search" },
+        { "azureaiservices", "Azure AI Services" },
+        { "azureapplicationinsights", "Azure Application Insights" },
+        { "azurebotservice", "Azure Bot Service" },
+        { "azurecosmosdb", "Azure Cosmos DB" },
+        { "azurekeyvault", "Azure Key Vault" },
+        { "azuredatabaseformysql", "Azure Database for MySQL" },
+        { "azureopenai", "Azure OpenAI" },
+        { "azuredatabaseforpostgresql", "Azure Database for PostgreSQL" },
+        { "azuresqldatabase", "Azure SQL Database" },
+        { "azurecacheforredis", "Azure Cache For Redis"},
+        { "azurestorageaccount", "Azure Storage Account" },
+        { "azureservicebus", "Azure Service Bus" },
+        { "azurewebpubsub", "Azure Web PubSub"}
+    };
+                                                                        
 }
 
 public enum NodeShape
--- tests/Areas/Deploy/UnitTests/ArchitectureDiagramTests.cs
+++ tests/Areas/Deploy/UnitTests/ArchitectureDiagramTests.cs
@@ -62,6 +62,55 @@ public class ArchitectureDiagramTests
                     {
                         new DependencyConfig { Name = "store", ConnectionType = "system-identity", ServiceType = "azurestorageaccount" }
                     },
+                },
+                new ServiceConfig
+                {
+                    Name = "frontend",
+                    Path = "testWorkspace/web",
+                    AzureComputeHost = "containerapp",
+                    Language = "js",
+                    Port = "8080",
+                    Dependencies = new DependencyConfig[]
+                    {
+                        new DependencyConfig { Name = "backend", ConnectionType = "http", ServiceType = "containerapp" }
+                    }
+                },
+                new ServiceConfig
+                {
+                    Name = "backend",
+                    Path = "testWorkspace/api",
+                    AzureComputeHost = "containerapp",
+                    Language = "python",
+                    Port = "3000",
+                    Dependencies = new DependencyConfig[]
+                    {
+                        new DependencyConfig { Name = "db", ConnectionType = "secret", ServiceType = "azurecosmosdb" },
+                        new DependencyConfig { Name = "secretStore", ConnectionType = "system-identity", ServiceType = "azurekeyvault" }
+                    }
+                },
+                new ServiceConfig
+                {
+                    Name = "frontendservice",
+                    Path = "testWorkspace/web",
+                    AzureComputeHost = "aks",
+                    Language = "ts",
+                    Port = "3001",
+                    Dependencies = new DependencyConfig[]
+                    {
+                        new DependencyConfig { Name = "backendservice", ConnectionType = "user-identity", ServiceType = "aks"}
+                    }
+                },
+                new ServiceConfig
+                {
+                    Name = "backendservice",
+                    Path = "testWorkspace/api",
+                    AzureComputeHost = "aks",
+                    Language = "python",
+                    Port = "3000",
+                    Dependencies = new DependencyConfig[]
+                    {
+                        new DependencyConfig { Name = "database", ConnectionType = "user-identity", ServiceType = "azurecacheforredis" }
+                    }
                 }
             }
         };
